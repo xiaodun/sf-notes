0196b50f717d397c09b8e265cc97c3c76653a175
wx <1947147481@qq.com>
2018-10-30 20:01:25 +0800

    没有开启内置服务器提示

--- tools_home/src/assets/lib/AxiosHelper.js
+++ tools_home/src/assets/lib/AxiosHelper.js
@@ -1,20 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 import builtService from '@root/service/app/config.json';
-var instance = axios.create({
-    baseURL:"/"+builtService.prefix,
+import {Message} from 'iView';
+var instance = axios.create ({
+  baseURL: '/' + builtService.prefix,
 });
 // instance.interceptors.request.use(function(){
 
 // })
-// instance.interceptors.response.use(function(){
-
-// })
+instance.interceptors.response.use (
+  function (response) {
+    return response;
+  },
+  (error, response) => {
+    if (error.response.status === 504) {
+      //没有开启内置服务器
+      Message.error ('请开启内置的服务器');
+    }
+  }
+);
 
 class Helper {
-    request(options){
-        return instance(options);
-    }
+  request (options) {
+    return instance (options);
+  }
 }
-var helper = new Helper();
+var helper = new Helper ();
 
 export default helper;
