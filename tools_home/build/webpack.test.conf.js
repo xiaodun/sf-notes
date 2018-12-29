4cd04f3c329aac1d992319574f87a461b8be513b
wx <1947147481@qq.com>
2018-12-29 09:45:11 +0800

    同步数据

--- tools_home/build/webpack.test.conf.js
+++ tools_home/build/webpack.test.conf.js
@@ -11,7 +11,7 @@ module.exports = {
   resolve: {
     alias: {
       vue$: 'vue/dist/vue.esm.js',
-      '@': path.resolve (__dirname, 'src'),
+      '@': path.resolve (__dirname, '...', 'src'),
     },
   },
   module: {
