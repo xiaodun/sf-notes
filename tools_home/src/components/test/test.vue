7ec39534c5d3bc9921aba7ace671040e79d39b95
xiaodun <1947147481@qq.com>
2019-01-06 16:08:30 +0800

    依赖移除
    sass install时会报错

--- tools_home/src/components/test/test.vue
+++ tools_home/src/components/test/test.vue
@@ -1,15 +1,13 @@
 <style lang="less">
-@import '~@/assets/style/base.less';
-
+@import "~@/assets/style/base.less";
 </style>
 <template>
   <div>
-    <achored-heading :level="2">
-      测试
-    </achored-heading>
+    <button @click="on_test">点击</button>
   </div>
 </template>
 <script>
+import AxiosHelper from "@/assets/lib/AxiosHelper";
 let num = 0;
 export default {
   name: "test_vue",
@@ -18,40 +16,21 @@ export default {
   },
 
   computed: {},
-  methods: {},
-  components: {
-    "achored-heading": {
-      render(crateElement) {
-        return crateElement("h" + this.level, [
-          this.$slots.default,
-          crateElement("span", [
-            12,
-            crateElement(
-              "a",
-              {
-                style: {
-                  color: "red"
-                },
-                class: [
-                  {
-                    foo: true
-                  },
-                  "age"
-                ],
-                attrs: {
-                  href: "http://baidu.com"
-                }
-              },
-              "456"
-            )
-          ])
-        ]);
-      },
-      props: {
-        level: Number
-      }
+  methods: {
+    async on_test() {
+      await this.request_get_tag().then(res => {
+        console.log(res);
+      });
+      console.log(this.response);
+    },
+    request_get_tag() {
+      return AxiosHelper.request({
+        method: "get",
+        url: "/personal/word/get"
+      });
     }
   },
+  components: {},
 
   mounted() {}
 };
