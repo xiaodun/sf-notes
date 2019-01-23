4819c395dff812b002ece39b1ca16efad95505ad
Xiao dun <1947147481@qq.com>
2019-01-23 14:15:02 +0800

    同步数据

--- tools_home/src/components/test/test.vue
+++ tools_home/src/components/test/test.vue
@@ -1,59 +1,57 @@
 
 
-<style lang="less" >
-@import "~@/assets/style/base.less";
-#test-vue-id {
-}
-</style>
+
 <template>
-  <div id="test-vue-id">
-    <textarea
-      name=""
-      id="testid"
-      cols="30"
-      rows="10"
-    >212121</textarea>
-    <test-com ref="dom"></test-com>
-    <button  @click="test">调用</button>
+  <div id="test-vue-id" style="height:1000px;">
+    <div class="child" v-for="(item,index) in colorList" :key="index" v-color="item"></div>
   </div>
 </template>
 <script>
 export default {
   name: "test_vue",
-  data() {
-    return {};
-  },
-  props: {},
-  computed: {},
-  methods: {
-    test(){
-      this.$emit('wait',12)
-    }
-  },
-  components: {
-    'test-com':{
-      template:`<div><button>broadcast调用</button><br/><button @click="on_dispatch">dispatch调用</button></div>`,
-      data(){
-        return{
-
-        }
-      },
-      methods:{
-        ref_say(){
-          alert('我被调用了')
-        },
-        on_dispatch(value){
-          this.$parent.$emit('wait')
-        }
+  components: {},
+  directives: {
+    color: {
+      bind(el, bind) {
+        let value = bind.value;
+        el.style.background = value;
+      }
+    },
+    scroll: {
+      bind(el, bind) {
+        let key = bind.value + "scroll";
+        let f = function() {};
       }
     }
   },
-  
-  mounted() {
-    this.$on("wait",function(value){
-      alert(value)
-    })
-  }
+  props: {},
+  data() {
+    return {
+      number: 1,
+      colorList: ["red", "green", "yellow", "black", "pink", "purple"]
+    };
+  },
+
+  computed: {},
+
+  methods: {
+    set() {
+      console.log(12);
+    }
+  },
+  watch: {},
+  mounted() {}
 };
 </script>
 
+<style lang="less" >
+@import "~@/assets/style/base.less";
+#test-vue-id {
+  .child {
+    width: 200px;
+    height: 0px;
+    margin: 10px auto;
+    transition: height ease-in-out 0.25s;
+  }
+}
+</style>
