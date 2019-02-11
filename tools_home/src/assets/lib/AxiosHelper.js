c370ab56173353cf1af72c846db7d002d4fa849a
Xiao dun <1947147481@qq.com>
2019-02-11 16:41:59 +0800

    优化ajax异常处理

--- tools_home/src/assets/lib/AxiosHelper.js
+++ tools_home/src/assets/lib/AxiosHelper.js
@@ -10,13 +10,15 @@ axios.interceptors.response.use(
     return response;
   },
   (error, response) => {
-    if (error.response.status === 504) {
+    if (error.response && error.response.status === 504) {
       //没有开启内置服务器
       if (isTip504 === false) {
         isTip504 = true;
         store.commit("change_app_state", false);
         Message.error("请开启内置的服务器");
       }
+    } else {
+      console.error(error);
     }
   }
 );
