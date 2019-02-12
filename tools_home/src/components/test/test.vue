70a77ffb03e36f521648b578f4b6ffed1e848200
Xiao dun <1947147481@qq.com>
2019-02-12 20:32:04 +0800

    记事本-优化过滤标签
    让过滤标签的弹窗可以向上弹出，从而不遮挡下面的过滤结果。
    
    解决因为滚动条导致的弹窗错位问题

--- tools_home/src/components/test/test.vue
+++ tools_home/src/components/test/test.vue
@@ -3,19 +3,11 @@
 
 <template>
   <div id="test-vue-id">
-    <button @click="isShow= true">测试</button>
-    <transition
-      appear
-      :css="false"
-      @before-enter="routerBeforeEnter"
-      @enter="routerEnter"
-      @after-enter="routerAfterEnter"
-    >
-      <div class="oop"></div>
-    </transition>
-    <!-- <transition name="slide-fade">
-      <div class="oop" v-if="isShow"></div>
-    </transition>-->
+    <Select placement="top" v-model="model6" style="width:200px">
+      <Option value="beijing">New York</Option>
+      <Option value="shanghai" disabled>London</Option>
+      <Option value="shenzhen">Sydney</Option>
+    </Select>
   </div>
 </template>
 <script>
@@ -26,29 +18,26 @@ export default {
   props: {},
   data() {
     return {
-      isShow: false
+      model6: null
     };
   },
 
   computed: {},
 
-  methods: {
-    routerBeforeEnter(el) {
-      el.style.marginTop = 0;
-    },
-    routerEnter(el, done) {
-      el.style.transition = "2.45s ease-in-out";
-
-      setTimeout(() => {
-        done();
-      });
-    },
-    routerAfterEnter(el) {
-      el.style.marginTop = 85 + "px";
+  methods: {},
+  watch: {},
+  computed: {
+    childsStr() {
+      console.log(this.childs);
+      return JSON.stringify(this.childs);
     }
   },
-  watch: {},
-  mounted() {}
+  mounted() {
+    let list = [1, null, undefined];
+    for (let i in list) {
+      console.log("wx", list[i]);
+    }
+  }
 };
 </script>
 
@@ -56,22 +45,5 @@ export default {
 @import "~@/assets/style/base.less";
 
 #test-vue-id {
-  margin-top: 85px;
-  .oop {
-    width: 200px;
-    height: 200px;
-    border: 1px solid #000;
-  }
-  .slide-fade-enter-active {
-    transition: all 0.3s ease;
-  }
-  .slide-fade-leave-active {
-    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-  }
-  .slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active for below version 2.1.8 */ {
-    transform: translateX(10px);
-    opacity: 0;
-  }
 }
 </style>
