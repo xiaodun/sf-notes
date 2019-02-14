8ff61f9d0e40b2b1bf5452d0b468ad943f24c40f
Xiao dun <1947147481@qq.com>
2019-02-14 18:32:34 +0800

    同步数据-五角星绘制

--- tools_home/src/components/test/canvas.vue
+++ tools_home/src/components/test/canvas.vue
@@ -3,6 +3,10 @@
 
 <template>
   <div id="canvas-vue-id">
+    <div>大圆的半径:</div>
+    <Slider v-model="shapeModel.bigR" :min="0" :max="200"></Slider>
+    <div>小圆的半径:</div>
+    <Slider v-model="shapeModel.smallR" :min="0" :max="200"></Slider>
     <canvas class="star-canvas" ref="canvasDom" width="400" height="400"></canvas>
   </div>
 </template>
@@ -13,42 +17,74 @@ export default {
 
   props: {},
   data() {
-    return {};
+    return {
+      _context: null,
+      shapeModel: {
+        bigR: 50,
+        hornNumber: 5,
+        bigStartDeg: 18,
+        smallStartDeg: 54,
+        smallR: 30,
+        isOpenSmall: true
+      }
+    };
   },
 
   computed: {},
 
-  methods: {},
-  watch: {},
+  methods: {
+    drawShape() {
+      let context = this.$data._context;
+      let clientRect = this.$refs.canvasDom.getBoundingClientRect();
+      context.clearRect(0, 0, clientRect.width, clientRect.height);
+      let {
+        hornNumber,
+        bigR,
+        bigStartDeg,
+        smallStartDeg,
+        smallR,
+        isOpenSmall
+      } = this.shapeModel;
+
+      context.beginPath();
+      context.save();
+      let maxR = Math.max(bigR, smallR);
+      context.translate(
+        clientRect.width / 2 - maxR,
+        clientRect.height / 2 - maxR
+      );
+      let list = Array(hornNumber).fill();
+      list.forEach((element, index) => {
+        let rad = (bigStartDeg + (360 / hornNumber) * index) * (Math.PI / 180);
+        let x = bigR - bigR * Math.cos(rad);
+        let y = bigR - bigR * Math.sin(rad);
+        context.lineTo(x, y);
+        if (isOpenSmall) {
+          let smallRad =
+            (smallStartDeg + (360 / hornNumber) * index) * (Math.PI / 180);
+          let distance = bigR - smallR;
+          let smallX = distance + smallR - smallR * Math.cos(smallRad);
+          let smallY = distance + smallR - smallR * Math.sin(smallRad);
+          context.lineTo(smallX, smallY);
+        }
+      });
+      context.restore();
+      context.closePath();
+      context.stroke();
+    }
+  },
+  watch: {
+    shapeModel: {
+      deep: true,
+      handler() {
+        this.drawShape();
+      }
+    }
+  },
   mounted() {
     let canvasDom = this.$refs.canvasDom;
-    let context = canvasDom.getContext("2d");
-
-    let hornNumber = 5;
-    let bigR = 50;
-
-    let strokeStyle = "yellow";
-    let startDeg = 18;
-    let smallR = 40;
-    let isOpenSmall = true;
-    context.beginPath();
-    context.save();
-    let list = Array(hornNumber).fill();
-    list.forEach((element, index) => {
-      let rad = (startDeg + (360 / hornNumber) * index) * (Math.PI / 180);
-      let x = bigR - bigR * Math.cos(rad);
-      let y = bigR - bigR * Math.sin(rad);
-      context.lineTo(x, y);
-      if (isOpenSmall) {
-        let distance = bigR - smallR;
-        let smallX = distance + smallR - smallR * Math.cos(rad);
-        let smallY = distance + smallR - smallR * Math.sin(rad);
-        context.lineTo(smallX, smallY);
-      }
-    });
-    context.restore();
-    context.closePath();
-    context.stroke();
+    this.$data._context = canvasDom.getContext("2d");
+    this.drawShape();
   }
 };
 </script>
