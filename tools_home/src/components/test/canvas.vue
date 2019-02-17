cb98e00b1d73d1d16d720771c47055eab61d91ca
xiaodun <1947147481@qq.com>
2019-02-17 21:30:28 +0800

    同步数据

--- tools_home/src/components/test/canvas.vue
+++ tools_home/src/components/test/canvas.vue
@@ -3,7 +3,11 @@
 
 <template>
   <div id="canvas-vue-id">
-    <canvas ref="canvasDom" width="400" height="400"></canvas>
+    <canvas
+      ref="canvasDom"
+      width="400"
+      height="400"
+    ></canvas>
   </div>
 </template>
 <script>
@@ -25,19 +29,27 @@ export default {
       context.beginPath();
       context.save();
       console.log(context);
-      let img = new Image();
-      img.src = "https://img.mukewang.com/images/unknow-80.png";
-      img.onload = () => {
-        let pattern = context.createPattern(img, "repeat");
-        context.fillStyle = pattern;
-        context.fillRect(0, 0, 400, 400);
-        context.restore();
-        context.closePath();
-        context.fill();
-      };
+      let backgroundCanvas = this.createBackgroundCanvas();
+
+      let pattern = context.createPattern(backgroundCanvas, "repeat-x");
+      context.fillStyle = pattern;
+      context.fillRect(0, 0, 400, 400);
+      context.restore();
+      context.closePath();
+    },
+    createBackgroundCanvas() {
+      let backgroundCanvas = document.createElement("canvas");
+      console.log(backgroundCanvas);
+      backgroundCanvas.width = 200;
+      backgroundCanvas.height = 200;
+      let backgroundContext = backgroundCanvas.getContext("2d");
+      backgroundContext.fillStyle = "red";
+      backgroundContext.fillRect(0, 0, 100, 100);
+      return backgroundCanvas;
     }
   },
   watch: {},
+
   mounted() {
     let canvasDom = this.$refs.canvasDom;
     this.$data._context = canvasDom.getContext("2d");
@@ -47,11 +59,12 @@ export default {
 </script>
 
 <style lang="less" >
-@import "~@/assets/style/base.less";
+@import '~@/assets/style/base.less';
 
 #canvas-vue-id {
   .star-canvas {
     border: 1px solid #000;
   }
 }
+
 </style>
--- tools_home/src/components/test/test.vue
+++ tools_home/src/components/test/test.vue
@@ -4,7 +4,10 @@
     <template>
   <div id="test-vue-id">
     <template v-if="list &&list.length > 0">
-      <div v-for="(item,index) in list" :key="index">{{index}}</div>
+      <div
+        v-for="(item,index) in list"
+        :key="index"
+      >{{index}}</div>
     </template>
     <div v-if="list && list.length === 0">暂无数据</div>
   </div>
@@ -19,13 +22,17 @@ export default {
     };
   },
   mounted() {
-    console.log(
-      ("00000" + ((Math.random() * 0x1000000) << 0).toString(16)).substr(-6)
-    );
+    for (let i = 0; i < 100; i++) {
+      let count = 4 * i - 1;
+      if ((count + 2) % 5 === 0) {
+        console.log(count);
+      }
+    }
   }
 };
 </script>
 
 <style lang="less" >
-@import "~@/assets/style/base.less";
+@import '~@/assets/style/base.less';
+
 </style>
