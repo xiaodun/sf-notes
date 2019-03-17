b98d1297491f92acd313e825e6dbbc05ef9bae36
xiaodun <1947147481@qq.com>
2019-03-17 22:06:13 +0800

    同步数据

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -3,11 +3,7 @@
 
 <template>
   <div id="test-vue-id" ref="testDom">
-    <mytbale v-bind:items="uesrs">
-      <template slot-scope="table">
-        <caption>{{table.user.name}}</caption>
-      </template>
-    </mytbale>
+    <div>1</div>
   </div>
 </template>
     <script>
@@ -16,38 +12,10 @@ export default {
 
   data() {
     return {
-      uesrs: [
-        { num: 1, name: "h", age: 12 },
-        { num: 2, name: "s", age: 13 },
-        { num: 3, name: "m", age: 14 }
-      ]
+      result: []
     };
   },
-  components: {
-    mytbale: {
-      template: `<table>
-                    <slot :user="user"></slot>
-                    <thead><th>序号</th><th>名字</th><th>年龄</th></thead>
-                    <tbody >
-                      <template v-for="item in items">
-                        <tr>
-                          <td>{{item.num}}</td>
-                          <td>{{item.name}}</td>
-                          <td>{{item.age}}</td>
-                        </tr>
-                      </template>
-                    </tbody>
-                  </table> `,
-      props: ["items"],
-      data() {
-        return {
-          user: {
-            name: "wx"
-          }
-        };
-      }
-    }
-  },
+  components: {},
   computed: {},
   methods: {},
   mounted() {}
