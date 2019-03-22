e9ca9debf7354d1e4c8a9f64eb5f50b36375e795
xiaodun <1947147481@qq.com>
2019-03-22 22:55:12 +0800

    记事本-提示用户微信内置浏览器不支持下载

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import iView from "iView";
 import "iview/dist/styles/iview.css";
 import store from "./vuex/store.js";
 import "@/assets/lib/AxiosHelper";
+import "@/assets/lib/Browser";
 Vue.use(iView);
 new Vue({
   router,
