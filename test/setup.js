4e3b61f9eab6cf72768618c38cc120551b498c8f
Xiao dun <1947147481@qq.com>
2019-04-02 15:51:34 +0800

    单元测试-记事本-功能点测试
    因为自己写的模块不支持require 和 import()这种方式，所以无法在setup中全局导入

--- test/setup.js
+++ test/setup.js
@@ -6,4 +6,5 @@ global.moxios = require("moxios");
 global.sinon = require("sinon");
 global.CryptoJS = require("crypto-js");
 global.Vue.use(global.iView);
-global.Vue.prototype.$browserMessage = import("@/assets/lib/Browser/Browser");
+//这里无法使用require和import 加载自己些的js模块
+global.Vue.prototype.$browserMessage = {};
