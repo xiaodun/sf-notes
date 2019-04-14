087de9c238fad7688e768b683d01e4e5456973c8
xiaodun <1947147481@qq.com>
2019-04-14 19:26:45 +0800

    同步数据

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -3,8 +3,31 @@
 
 <template>
   <div id="test-vue-id" ref="testDom">
-    <span style="border:1px solid red;">123</span>
-    <button class="antd-btn" @animationend="end" @click="test">测试</button>
+    <Slider v-model="offset" :min="10" :max="314"></Slider>
+    <svg width="100%" height="100%" viewBox="0 0 100 100">
+      <defs>
+        <linearGradient id="linearGradient-border" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" style="stop-color:rgb(255,0,0)"></stop>
+          <stop offset="100%" style="stop-color:rgb(0,255,0)"></stop>
+        </linearGradient>
+      </defs>
+      <path
+        d="M 50,50 m 0,-40
+                a 40,40 0 0 0 0,80 
+                 a 40,40 0 0 0 0,-80  "
+        stroke-width="5"
+        stroke-dasharray="251"
+        :stroke-dashoffset="offset"
+        style="stroke:url(#linearGradient-border);fill:transparent;"
+      ></path>
+      <circle id="circle-id" cx="50" cy="10" r="10" style="transform-origin:50px 50px;
+ transform: rotate(-60deg);">
+        >
+      </circle>
+    </svg>
+ 
+</svg>
+    
   </div>
 </template>
 
@@ -15,32 +38,20 @@ export default {
 
   data() {
     return {
-      result: []
+      offset: 100
     };
   },
   components: {},
   computed: {},
   methods: {},
-  mounted() {
-    let total = 20000000;
-    let a = 1,
-      b = 3;
-    let times = 1;
-    let count = b;
-    do {
-      times++;
-      let temp = a;
-      count += a;
-      a = a + b;
-      b = temp;
-      console.log(count, a, b);
-    } while (count < total);
-    console.log(times);
-  }
+  mounted() {}
 };
 </script>
 
 <style lang="less" >
-@import '~@/assets/style/base.less';
-
+@import "~@/assets/style/base.less";
+#test-vue-id{
+  width:200px;
+  height:200px;
+}
 </style>
