068fb0b8265f7801b233164c17f74a74702567be
Xiao dun <1947147481@qq.com>
2019-04-16 20:49:58 +0800

    同步数据

--- service/app/service.js
+++ service/app/service.js
@@ -1,4 +1,5 @@
 var Service = require("sf-inner-service");
 var file_os = require("fs");
+var a = 10;
 let config = JSON.parse(file_os.readFileSync("config.json", "utf-8"));
 Service.start(config);
--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -3,31 +3,43 @@
 
 <template>
   <div id="test-vue-id" ref="testDom">
-    <Slider v-model="offset" :min="10" :max="314"></Slider>
+您的浏览器不支持 HTML5 canvas 标签。</canvas>
+    <Slider v-model="percent" :min="0" :max="100"></Slider>
+    
     <svg width="100%" height="100%" viewBox="0 0 100 100">
       <defs>
         <linearGradient id="linearGradient-border" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" style="stop-color:rgb(255,0,0)"></stop>
-          <stop offset="100%" style="stop-color:rgb(0,255,0)"></stop>
+          <stop offset="0%" :style="{
+            'stop-color':this.startColor
+          }"></stop>
+          <stop offset="100%" :style="{
+            'stop-color':this.endColor
+          }"></stop>
         </linearGradient>
       </defs>
       <path
-        d="M 50,50 m 0,-40
-                a 40,40 0 0 0 0,80 
-                 a 40,40 0 0 0 0,-80  "
+        :d="pathStrings"
         stroke-width="5"
-        stroke-dasharray="251"
+        :stroke-dasharray="circumference"
         :stroke-dashoffset="offset"
-        style="stroke:url(#linearGradient-border);fill:transparent;"
+        :style="{
+          'stroke':'url(#linearGradient-border)','fill':'transparent',
+          'transition':this.transition
+        }"
       ></path>
-      <circle id="circle-id" cx="50" cy="10" r="10" style="transform-origin:50px 50px;
- transform: rotate(-60deg);">
-        >
-      </circle>
+      <circle
+        id="circle-id"
+        cx="50"
+        :cy="boredrWidth"
+        :r="dotRadius"
+        :style="{
+        "transform-origin":"50px 50px",
+        "transform": `rotate(${this.dotRotateDeg}deg)`,
+        "fill":this.dotBgColor,
+        "transition":this.transition
+      }"
+      >></circle>
     </svg>
- 
-</svg>
-    
   </div>
 </template>
 
@@ -38,20 +50,67 @@ export default {
 
   data() {
     return {
-      offset: 100
+      transition: "linear 0.6s",
+      dotBgColor: "rgb(255,80,80)",
+      percent: 10,
+      dotRadius: 5,
+      startColor: "rgb(255,0,0)",
+      endColor: "rgb(0,255,255)",
+      boredrWidth: 10
     };
   },
   components: {},
-  computed: {},
+  computed: {
+    dotRotateDeg() {
+      return (
+        (-180 * ((this.percent / 100) * this.circumference)) /
+        (Math.PI * this.radius)
+      );
+    },
+    offset() {
+      return this.circumference - (this.percent / 100) * this.circumference;
+    },
+    circumference() {
+      return 2 * Math.PI * this.radius;
+    },
+    radius() {
+      return 50 - this.boredrWidth;
+    },
+    pathStrings() {
+      return `M 50,50 m 0,-${this.radius}
+                a ${this.radius},${this.radius} 0 0 0 0,${2 * this.radius} 
+                 a ${this.radius},${this.radius} 0 0 0 0,-${2 * this.radius}`;
+    }
+  },
   methods: {},
-  mounted() {}
+  mounted() {
+      let arr = [1,3,4,10,8,7,9,13,15,89,1];
+      function sort(argList){
+        if(argList.length <= 1){
+          return argList;
+        }
+        let leftList = [],rightList=[],middle = argList.splice(Math.floor(argList.length/2),1)[0];
+        
+        argList.forEach((el,index,arr)=>{
+
+          if(el<middle){
+            leftList.push(el);
+          }
+          else{
+            rightList.push(el)
+          }
+        })
+        return sort(leftList).concat(middle,sort(rightList))
+      }
+      console.log(sort(arr))
+  }
 };
 </script>
 
 <style lang="less" >
 @import "~@/assets/style/base.less";
-#test-vue-id{
-  width:200px;
-  height:200px;
+#test-vue-id {
+  width: 200px;
+  height: 200px;
 }
 </style>
