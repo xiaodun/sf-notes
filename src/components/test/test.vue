f1d11ba83d2c372094cd3b76c59d024b9a4a8be3
wx <1947147481@qq.com>
2019-06-14 19:06:09 +0800

    同步数据

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -2,12 +2,49 @@
 
 
 <template>
-  <div id="test-vue-id" ref="testDom">
-    <span>123</span>
+  <div
+    id="test-vue-id"
+    ref="testDom"
+  >
+    <button @click="onEmit()">emit</button>
+    <button @click="onOff()">off</button>
   </div>
 </template>
 
     <script>
+class EventTools {
+  callBackObj = {};
+  on(argKey, argCallback, isOne) {
+    if (this.callBackObj.argKey === undefined) {
+      this.callBackObj.argKey = new Set();
+    }
+    this.callBackObj.argKey.add(argCallback);
+    console.log(this.callBackObj.argKey);
+  }
+  emit(argKey) {
+    if (this.callBackObj.argKey) {
+      for (let callBack of this.callBackObj.argKey) {
+        callBack(...[...arguments].slice(1));
+      }
+    }
+  }
+  off(argKey, argCallback) {
+    if (this.callBackObj.argKey) {
+      this.callBackObj.argKey.delete(argCallback);
+      console.log(this.callBackObj.argKey);
+    }
+  }
+  once(argKey, argCallback) {
+    let _this = this;
+    let fn = function() {
+      argCallback.apply(null, arguments);
+
+      _this.off(argKey, fn);
+    };
+    this.on(argKey, fn);
+  }
+}
+let eventTools = new EventTools();
 export default {
   name: "test_vue",
 
@@ -16,26 +53,21 @@ export default {
   },
   components: {},
   computed: {},
-  methods: {},
+  methods: {
+    onEmit() {
+      eventTools.emit("key", 12, 13);
+    },
+    onOff() {
+      eventTools.off("key", this.test);
+    },
+    test() {
+      console.log(arguments);
+    }
+  },
 
   mounted() {
-    var obj1 = {
-      name: "obj1"
-    };
-    var obj2 = {
-      name: "obj2"
-    };
-    var obj3 = {
-      name: "obj3"
-    };
-    function say() {
-      console.log(this.name);
-    }
-    var fn = say
-      .bind(obj1)
-      .bind(obj2)
-      .bind(obj3);
-    fn();
+    eventTools.once("key", this.test, true);
+    eventTools.on("key", this.test);
   }
 };
 </script>
@@ -48,8 +80,12 @@ export default {
 
   width: 100%;
   height: 400px;
+  margin: ~"calc((@{layout-header-height} - 24px) / 2)" 0;
+
+  @layout-header-height: 100px;
 
   main {
   }
 }
+
 </style>
