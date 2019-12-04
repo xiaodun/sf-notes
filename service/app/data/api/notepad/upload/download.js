8ec3fcfae18004b2631b542d66f668f35d3d1f7e
xiaodun <1947147481@qq.com>
2019-12-04 23:19:46 +0800

    记事本-优化下载逻辑
    解决手机端删除文件后，在PC下载这个文件服务器退出的问题

--- service/app/data/api/notepad/upload/download.js
+++ service/app/data/api/notepad/upload/download.js
@@ -3,16 +3,33 @@
     //argData 数据的副本
     let id = argParams.id;
     let file = '';
-    argData.some ((el, index, arr) => {
+    argData.some((el, index, arr) => {
       if (el.id === id) {
         file = {
           flag: el.flag,
           name: el.name,
-          type:el.type
+          type: el.type
         };
         return true;
       }
     });
+    if (!file) {
+      //文件不存在
+      /**
+       * 在手机端登陆删除了文件  又在pc端点击了下载
+       */
+      return {
+        isWrite: false, //是否覆盖数据
+        response: {
+          //返回的数据
+          code: 200,
+          data: {
+            flag: -1
+          },
+        },
+      };
+
+    }
     return {
       isWrite: false, //是否覆盖数据
       //data:argData,//需要存储的新数据
@@ -25,4 +42,4 @@
       },
     };
   };
-}) ();
+})();
--- src/components/tools/notepad/file_manager.vue
+++ src/components/tools/notepad/file_manager.vue
@@ -4,7 +4,7 @@
 #file_manager-vue-id {
   .uploading-enter-active,
   .uploading-leave-active {
-    transition: all .5s;
+    transition: all 0.5s;
   }
 
   .uploading-enter,
@@ -41,7 +41,7 @@
 
       border-bottom: 1px solid #ccc;
 
-.vertical_lineheight(32px);
+      .vertical_lineheight(32px);
     }
 
     .option {
@@ -129,14 +129,10 @@
     }
   }
 }
-
 </style>
 <template>
   <div id="file_manager-vue-id">
-    <Button
-      class="first-btn"
-      @click="$emit('on-back')"
-    >返回</Button>
+    <Button class="first-btn" @click="$emit('on-back')">返回</Button>
     <Alert v-if="$browserMessage.isWeChat">微信内置浏览器不支持下载!</Alert>
     <Upload
       :on-progress="onUploadProgress"
@@ -154,76 +150,59 @@
     <!-- 上传 -->
     <div class="upload-wrapper">
       <!-- 正在上传的文件 -->
-      <transition-group
-        tag="div"
-        name="uploading"
-      >
+      <transition-group tag="div" name="uploading">
         <div
           class="uploading"
           :key="index"
-          v-for="(item,index) in uploadingList"
+          v-for="(item, index) in uploadingList"
         >
-          <div>{{item.name}}</div>
+          <div>{{ item.name }}</div>
           <Progress :percent="item.percent" />
         </div>
       </transition-group>
       <!-- 已经上传完毕 -->
-      <div
-        class="file"
-        :key="index"
-        v-for="(item,index) in uploadList"
-      >
+      <div class="file" :key="index" v-for="(item, index) in uploadList">
         <Row>
           <Col span="10">
-          <div class="name">{{item.name}}</div>
+            <div class="name">{{ item.name }}</div>
           </Col>
-          <Col
-            class="option"
-            span="12"
-            offset="1"
-          >
-          <Button
-            v-if="!$browserMessage.isWeChat"
-            icon="md-download"
-            :loading="item.isDownloading"
-            style="margin-right:10px"
-            title="下载"
-            shape="circle"
-            @click="onDownload(item)"
-          ></Button>
-          <Button
-            shape="circle"
-            icon="md-remove"
-            title="删除"
-            style="margin-right:10px"
-            @click="onConfirmDelete(item)"
-          ></Button>
+          <Col class="option" span="12" offset="1">
+            <Button
+              v-if="!$browserMessage.isWeChat"
+              icon="md-download"
+              :loading="item.isDownloading"
+              style="margin-right:10px"
+              title="下载"
+              shape="circle"
+              @click="onDownload(item)"
+            ></Button>
+            <Button
+              shape="circle"
+              icon="md-remove"
+              title="删除"
+              style="margin-right:10px"
+              @click="onConfirmDelete(item)"
+            ></Button>
 
-          <Button
-            shape="circle"
-            title="备注"
-            icon="md-information"
-            style="margin-right:10px"
-            @click="inUpdateModal(item)"
-          ></Button>
-          <Button
-            shape="circle"
-            icon="md-eye"
-            title="预览"
-            @click="onPreviewFile(item)"
-          ></Button>
+            <Button
+              shape="circle"
+              title="备注"
+              icon="md-information"
+              style="margin-right:10px"
+              @click="inUpdateModal(item)"
+            ></Button>
+            <Button
+              shape="circle"
+              icon="md-eye"
+              title="预览"
+              @click="onPreviewFile(item)"
+            ></Button>
           </Col>
-          <Col
-            span="14"
-            v-if="item.describe"
-          >
-          <div class="fileinfo">{{item.describe}}</div>
+          <Col span="14" v-if="item.describe">
+            <div class="fileinfo">{{ item.describe }}</div>
           </Col>
-          <Col
-            span="14"
-            v-if="item.describe"
-          >
-          <div class="fileinfo">{{item.describe}}</div>
+          <Col span="14" v-if="item.describe">
+            <div class="fileinfo">{{ item.describe }}</div>
           </Col>
         </Row>
       </div>
@@ -253,59 +232,48 @@
     >
       <div class="container">
         <Row class="row">
-          <Col
-            span="4"
-            class="label"
-          >名称:</Col>
+          <Col span="4" class="label">名称:</Col>
           <Col span="20">
-          <div class="name">{{activePreview.name}}</div>
+            <div class="name">{{ activePreview.name }}</div>
           </Col>
         </Row>
         <Row class="row">
-          <Col
-            span="4"
-            class="label"
-          >类型:</Col>
+          <Col span="4" class="label">类型:</Col>
           <Col span="20">
-          <RadioGroup
-            v-model="activePreview.type"
-            @on-change="onChangFileType()"
-          >
-            <Radio
-              :disabled="computedDisabledFileRadio"
-              :label="FileType.txt"
-            >文本</Radio>
-            <Radio
-              :disabled="computedDisabledFileRadio"
-              :label="FileType.img"
-            >图片</Radio>
-            <Radio
-              :disabled="computedDisabledFileRadio"
-              :label="FileType.audio"
-            > 音频</Radio>
-            <Radio
-              :disabled="computedDisabledFileRadio"
-              :label="FileType.video"
-            >视频</Radio>
-          </RadioGroup>
+            <RadioGroup
+              v-model="activePreview.type"
+              @on-change="onChangFileType()"
+            >
+              <Radio :disabled="computedDisabledFileRadio" :label="FileType.txt"
+                >文本</Radio
+              >
+              <Radio :disabled="computedDisabledFileRadio" :label="FileType.img"
+                >图片</Radio
+              >
+              <Radio
+                :disabled="computedDisabledFileRadio"
+                :label="FileType.audio"
+              >
+                音频</Radio
+              >
+              <Radio
+                :disabled="computedDisabledFileRadio"
+                :label="FileType.video"
+                >视频</Radio
+              >
+            </RadioGroup>
           </Col>
         </Row>
         <Row class="row" v-if="activePreview.type === FileType.txt">
-          <Col
-            span="4"
-            class="label"
-          >编码:</Col>
+          <Col span="4" class="label">编码:</Col>
           <Col span="10">
-          <Input
-            v-model="defaultEncode"
-            @on-keyup.enter="onParseFile(FileType.txt)"
-          ></Input>
+            <Input
+              v-model="defaultEncode"
+              @on-keyup.enter="onParseFile(FileType.txt)"
+            ></Input>
           </Col>
-          <Col
-            offset="1"
-            span="4"
-          >
-          <Button @click="onParseFile(FileType.txt)">解析</Button>
+          <Col offset="1" span="4">
+            <Button @click="onParseFile(FileType.txt)">解析</Button>
           </Col>
         </Row>
         <Row>
@@ -313,43 +281,21 @@
             解析中...
           </div>
           <div v-else>
-
             <div v-if="activePreview.type">
-              <div
-                class="txt"
-                v-if="activePreview.type === FileType.txt"
-              >
-                {{activePreview.txtContent}}
+              <div class="txt" v-if="activePreview.type === FileType.txt">
+                {{ activePreview.txtContent }}
               </div>
-              <div
-                class="img"
-                v-if="activePreview.type === FileType.img"
-              >
-                <img :src="activePreview.imgSrc">
+              <div class="img" v-if="activePreview.type === FileType.img">
+                <img :src="activePreview.imgSrc" />
               </div>
-              <div
-                class="audio"
-                v-if="activePreview.type === FileType.audio"
-              >
-                <audio
-                  controls
-                  :src="activePreview.audioSrc"
-                ></audio>
+              <div class="audio" v-if="activePreview.type === FileType.audio">
+                <audio controls :src="activePreview.audioSrc"></audio>
               </div>
-              <div
-                class="video"
-                v-if="activePreview.type === FileType.video"
-              >
-                <video
-                  controls
-                  :src="activePreview.videoSrc"
-                ></video>
+              <div class="video" v-if="activePreview.type === FileType.video">
+                <video controls :src="activePreview.videoSrc"></video>
               </div>
             </div>
-            <div
-              v-else
-              class="no-preview"
-            >
+            <div v-else class="no-preview">
               该文件不在内置的可预览的文件类型中!可自行尝试!
             </div>
           </div>
@@ -363,21 +309,22 @@
 let fileToBlob = {
   //缓存文件数据
 };
+import _ from "lodash";
 import BuiltServiceConfig from "@root/service/app/config.json";
 import { FileType, StuffixWithType, getFileType } from "@/assets/lib/preview";
 export default {
   name: "file_manager_vue",
   model: {
     prop: "uploadList",
-    event: "change"
+    event: "change",
   },
   props: {
     uploadList: {
       type: Array,
       default: function() {
         return [];
-      }
-    }
+      },
+    },
   },
   data() {
     return {
@@ -392,7 +339,7 @@ export default {
         imgSrc: "", //图片内容,
         videoSrc: "", //视频内容
         audioSrc: "", //音频内容
-        isLoading: false //是否处于解析状态
+        isLoading: false, //是否处于解析状态
       },
       BuiltServiceConfig,
       requestPrefix: "/notepad/upload", //上传文件请求前缀
@@ -400,7 +347,7 @@ export default {
       isPreviewFile: false, //预览文件模态框
       isShowUpdate: false,
       FileType,
-      StuffixWithType
+      StuffixWithType,
     };
   },
   methods: {
@@ -447,14 +394,14 @@ export default {
       switch (argType) {
         case FileType.txt:
           fileReader.readAsText(data, this.defaultEncode);
-          fileReader.onload = event => {
+          fileReader.onload = (event) => {
             this.activePreview.txtContent = fileReader.result;
             this.activePreview.isLoading = false;
           };
           break;
         case FileType.img:
           fileReader.readAsDataURL(data);
-          fileReader.onload = event => {
+          fileReader.onload = (event) => {
             this.activePreview.isLoading = false;
             this.activePreview.imgSrc = fileReader.result;
           };
@@ -494,8 +441,8 @@ export default {
         url: this.requestPrefix + "/update",
         data: {
           id: argItem.id,
-          describe: argItem.describe
-        }
+          describe: argItem.describe,
+        },
       });
     },
     inUpdateModal(argItem) {
@@ -507,7 +454,7 @@ export default {
       });
     },
     onUploadProgress(argEvent, argFile) {
-      let index = this.uploadingList.findIndex(el => el.file === argFile);
+      let index = this.uploadingList.findIndex((el) => el.file === argFile);
 
       if (index === -1) {
         let uploadingFile = {};
@@ -529,13 +476,29 @@ export default {
       argItem.isDownloading = true;
       let response = await this.requestDownload(argItem);
       var blob = response.data;
-      var a = document.createElement("a");
-      a.download = argItem.name;
-      a.href = URL.createObjectURL(blob);
-      document.body.appendChild(a);
-      a.click();
-      a.remove();
-      URL.revokeObjectURL(a.href);
+      if (blob.type === "application/json") {
+        const reader = new FileReader();
+        reader.readAsText(blob, "utf-8");
+        reader.onload = () => {
+          let data = JSON.parse(reader.result);
+          if (data.flag === -1) {
+            this.$Message.error("该文件已经被删除了");
+            //移除这个文件
+            _.pullAllBy(this.uploadList, [{ id: argItem.id }], "id");
+
+            this.$emit("change", [...this.uploadList]);
+          }
+        };
+      } else {
+        var a = document.createElement("a");
+        a.download = argItem.name;
+        a.href = URL.createObjectURL(blob);
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(a.href);
+      }
+
       argItem.isDownloading = false;
     },
     requestDownload(argItem) {
@@ -543,7 +506,7 @@ export default {
       return this.$axios.request({
         method: "get",
         url: this.requestPrefix + "/download" + `?id=${argItem.id}`,
-        responseType: "blob"
+        responseType: "blob",
       });
     },
     onConfirmDelete(argItem) {
@@ -552,7 +515,7 @@ export default {
         content: "删除文件后,无法通过界面操作恢复!",
         onOk: () => {
           this.onDelete(argItem);
-        }
+        },
       });
     },
     async onDelete(argItem) {
@@ -566,8 +529,8 @@ export default {
         method: "post",
         url: this.requestPrefix + "/delete",
         data: {
-          id: argItem.id
-        }
+          id: argItem.id,
+        },
       });
     },
     async onGet() {
@@ -582,15 +545,15 @@ export default {
       //获取文件
       return this.$axios.request({
         method: "get",
-        url: this.requestPrefix + "/get"
+        url: this.requestPrefix + "/get",
       });
-    }
+    },
   },
   computed: {
     computedDisabledFileRadio() {
       let isDisabled = this.activeFile.isLoading || !this.isCanTry;
       return isDisabled;
-    }
+    },
   },
   beforeDestroy() {
     //卸载组件时清空数据,fileToBlob由于不在组件内部,所以需要手动清空
@@ -598,6 +561,6 @@ export default {
   },
   mounted() {
     this.onGet();
-  }
+  },
 };
 </script>
