f7a3c98fea7ed24db22fa54c3f014a46f3b91c72
xiaodun <1947147481@qq.com>
2019-12-18 13:38:41 +0800

    同步数据

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -3,7 +3,10 @@
 
 <template>
   <div id="test-vue-id" class="FlexWrapper" wrapper column ref="testDom">
-    
+    <Button @click="generateList"> 生成10个随机高度的子元素</Button>
+    <div class="parent" style="display:none;">
+      <div v-for="item in list" :key="item.id" :style="item.style"></div>
+    </div>
   </div>
 </template>
     <script>
@@ -14,15 +17,40 @@ export default {
 
   data() {
     return {
-      list: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      list: [],
     };
   },
 
   components: {},
   computed: {},
-  methods: {},
+  methods: {
+    generateList() {
+      this.list = Array(10)
+        .fill(1)
+        .map(() => {
+          let child = {
+            id: _.uniqueId(),
+            style: {
+              height: _.random(120) + "px",
+              "margin-top": _.random(20) + "px",
+              "background-color": "red",
+            },
+          };
+          return child;
+        });
+    },
+  },
 
-  mounted() {},
+  mounted() {
+    this.generateList();
+    // (function() {
+    //   $0.style.display = "block";
+    //   console.log($0.getBoundingClientRect());
+
+    //   $0.style.display = "none";
+    // })();
+    console.log("wx");
+  },
 };
 </script>
 
