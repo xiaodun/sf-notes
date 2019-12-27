08e405a8d79a2128655eb5eab0a60b37b8ece39e
Xiao dun <1947147481@qq.com>
2019-12-27 12:07:11 +0800

    记事本-添加记事后不清除标签

--- src/components/tools/notepad/notepad.vue
+++ src/components/tools/notepad/notepad.vue
@@ -406,7 +406,7 @@ isDecripty:fasle  标记当前文本状态 是否在客户端被解密了
         content: "",
         loadCount: 0, //用户黏贴图片的时候记录正在转换的图片个数,loadCount为0时,才可以提交图片
         base64: {},
-        tagId:this.filterTagId
+        tagId: this.filterTagId,
       };
       this.isShowAddModel = true;
     },
@@ -595,7 +595,6 @@ isDecripty:fasle  标记当前文本状态 是否在客户端被解密了
       this.isShowEditModel = false;
     },
     async onAdd(argNotepad) {
-      this.filterTagId = "";
       await this.requestAdd(argNotepad);
       this.pagination.page = 1;
       this.onGet(this.pagination);
