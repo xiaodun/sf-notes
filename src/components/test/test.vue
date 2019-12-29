fc3558500870b92ee33b4b31e08c7120c0514622
xiaodun <1947147481@qq.com>
2019-12-29 15:42:57 +0800

    同步数据

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -3,10 +3,8 @@
 
 <template>
   <div id="test-vue-id" class="FlexWrapper" wrapper column ref="testDom">
-    <Button @click="generateList"> 生成10个随机高度的子元素</Button>
-    <div class="parent" style="display:none;">
-      <div v-for="item in list" :key="item.id" :style="item.style"></div>
-    </div>
+    
+ 
   </div>
 </template>
     <script>
@@ -24,40 +22,11 @@ export default {
   components: {},
   computed: {},
   methods: {
-    generateList() {
-      this.list = Array(10)
-        .fill(1)
-        .map(() => {
-          let child = {
-            id: _.uniqueId(),
-            style: {
-              height: _.random(120) + "px",
-              "margin-top": _.random(20) + "px",
-              "background-color": "red",
-            },
-          };
-          return child;
-        });
-    },
+   
   },
 
   mounted() {
-    (function() {
-      const apiVersion = "v1";
-      const prefix = "api";
-      const mapUrlPattern = /{(.*?)}/g;
-      const mapUrlObj = {
-        apiVersion,
-        prefix,
-      };
-      let url = "{prefix}/door/{apiVersion}/access/room/getAreaRoomInfoTree";
-      url = url.replace(mapUrlPattern, (all, key) => {
-        let value = mapUrlObj[key] == undefined ? all : mapUrlObj[key];
-        return value;
-      });
-
-      console.log(url);
-    })();
+    
   },
 };
 </script>
@@ -65,6 +34,7 @@ export default {
 <style lang="less" >
 @import "~@/assets/style/base.less";
 
-#test-vue-id {
-}
+#test-vue-id {}
+  
+
 </style>
