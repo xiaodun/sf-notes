0c6639a510345b7747a382cf0754710b33aad336
Xiao dun <1947147481@qq.com>
2020-01-06 14:52:19 +0800

    移除分支说明

--- src/components/tools/notepad/notepad.vue
+++ src/components/tools/notepad/notepad.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="notepad-id">
     <div class="wrapper">
-      <Alert>feature 分支</Alert>
       <div class="card-wrapper" v-if="showModelFlag === 'notepad'">
         <Button
           icon="ios-pricetag"
