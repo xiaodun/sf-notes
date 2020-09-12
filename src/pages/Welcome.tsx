764fd1a07c1b63fb416a46fda637d460039b6bfb
Xiao dun <1947147481@qq.com>
2020-09-13 00:06:39 +0800

    新增记录问题文件

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -8,13 +8,13 @@ export default () => {
   const [time, setTime] = useState('');
   const timeRef = useRef<number>();
   useEffect(() => {
-    timeRef.current = window.setInterval(() => {
-      const timeStr = moment().format(hh_mm_ss);
-      setTime(timeStr);
-    }, 1000);
-    return () => {
-      window.clearInterval(timeRef.current);
-    };
+    // timeRef.current = window.setInterval(() => {
+    //   const timeStr = moment().format(hh_mm_ss);
+    //   setTime(timeStr);
+    // }, 1000);
+    // return () => {
+    //   window.clearInterval(timeRef.current);
+    // };
   }, []);
   return (
     <Layout className={SelfStyle.layput}>
