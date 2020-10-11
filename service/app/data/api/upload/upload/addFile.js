9a7d82e516f13336409eeae179d80a6f8671d0a4
Xiao dun <1947147481@qq.com>
2020-10-11 21:42:52 +0800

    js文件需要上传

---
+++ service/app/data/api/upload/upload/addFile.js
@@ -0,0 +1,18 @@
+(function () {
+  return function (argData, argParams) {
+    //argData 数据的副本
+    argData = argData || [];
+    const file = argParams.files[0];
+    argData.unshift(file);
+    file.id = +new Date() + '';
+    return {
+      isWrite: true, //是否覆盖数据
+      data: argData, //需要存储的新数据
+      response: {
+        //返回的数据
+        code: 200,
+        data: file,
+      },
+    };
+  };
+})();
