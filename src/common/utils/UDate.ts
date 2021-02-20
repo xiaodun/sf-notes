e3e04866036b138da52672b05bc8c42caf628707
Xiao dun <1947147481@qq.com>
2021-02-21 00:34:31 +0800

    时间格式化

--- src/common/utils/UDate.ts
+++ src/common/utils/UDate.ts
@@ -1,5 +1,14 @@
+import moment from 'moment';
+
 export namespace UDate {
   export const ymd = 'YYYY-MM-DD';
   export const hms = 'hh:mm:ss';
+  export const ymdhms = ymd + ' ' + hms;
+  export function fomat(timestamp: number, pattern: string) {
+    if (timestamp == undefined) {
+      return '';
+    }
+    return moment(timestamp).format(pattern);
+  }
 }
 export default UDate;
