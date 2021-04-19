c6fac9869773015561879af6f9b91b433e3c9831
王旭 <1947147481@qq.com>
2021-04-19 16:05:32 +0800

    支持向下插入

--- src/pages/book/components/list/BookTitleDrawer.tsx
+++ src/pages/book/components/list/BookTitleDrawer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { connect, ConnectRC, NMDBook, NMDGlobal } from "umi";
-import { Button, Drawer, Input, Menu } from "antd";
+import { Button, Drawer, Dropdown, Menu } from "antd";
 import SelfStyle from "./BookTitleDrawer.less";
 import NModel from "@/common/namespace/NModel";
 import NBook from "../../NBook";
 import SBook from "../../SBook";
-import SubMenu from "_antd@4.15.1@antd/lib/menu/SubMenu";
 import qs from "qs";
 import NRouter from "@/../config/router/NRouter";
+import { EllipsisOutlined } from "@ant-design/icons";
 export interface IBookTitleDrawerProps {
   MDGlobal: NMDGlobal.IState;
   MDBook: NMDBook.IState;
@@ -36,15 +36,15 @@ const BookTitleDrawer: ConnectRC<IBookTitleDrawerProps> = (props) => {
         <Menu.Item key={urlQuery.id} onClick={() => goEdit(null, "book")}>
           基本信息
         </Menu.Item>
-        <SubMenu key="preface" title="序言">
+        <Menu.SubMenu key="preface" title="序言">
           {renderPieceList(props.MDBook.book.prefaceList, "preface")}
-        </SubMenu>
-        <SubMenu key="chapter" title="章节">
+        </Menu.SubMenu>
+        <Menu.SubMenu key="chapter" title="章节">
           {renderPieceList(props.MDBook.book.chapterList, "chapter")}
-        </SubMenu>
-        <SubMenu key="epilog" title="结语">
+        </Menu.SubMenu>
+        <Menu.SubMenu key="epilog" title="结语">
           {renderPieceList(props.MDBook.book.epilogList, "epilog")}
-        </SubMenu>
+        </Menu.SubMenu>
       </Menu>
     </Drawer>
   );
@@ -94,16 +94,40 @@ const BookTitleDrawer: ConnectRC<IBookTitleDrawerProps> = (props) => {
       return (
         <>
           {renderAddMenuItem(0)}
-          {list.map((item) => (
-            <Menu.Item
-              onClick={() => {
-                goEdit(item.id, updateType);
-              }}
-              key={item.id}
-            >
-              {item.title || "暂无标题"}
-            </Menu.Item>
-          ))}
+          {list.map((item, index) => {
+            const menu = (
+              <Menu>
+                <Menu.Item
+                  onClick={() => reqCreatBookPiece(updateType, index + 1)}
+                >
+                  向下插入
+                </Menu.Item>
+              </Menu>
+            );
+            return (
+              <Menu.Item key={item.id}>
+                <div className={SelfStyle.menuItem}>
+                  <span
+                    className={SelfStyle.title}
+                    onClick={() => {
+                      goEdit(item.id, updateType);
+                    }}
+                  >
+                    {item.title || "暂无标题"}
+                  </span>
+                  <Dropdown
+                    trigger={["click"]}
+                    className={SelfStyle.dropdown}
+                    overlay={menu}
+                  >
+                    <Button size="small" className="ant-dropdown-link">
+                      <EllipsisOutlined />
+                    </Button>
+                  </Dropdown>
+                </div>
+              </Menu.Item>
+            );
+          })}
           {renderAddMenuItem(list.length)}
         </>
       );
