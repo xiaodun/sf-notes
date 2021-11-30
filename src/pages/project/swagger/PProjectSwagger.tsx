2d47aac2c40044d57327366a1b8d8e41bb519af5
王旭 <1947147481@qq.com>
2021-11-30 18:31:44 +0800

    feat: 显示接口菜单

--- src/pages/project/swagger/PProjectSwagger.tsx
+++ src/pages/project/swagger/PProjectSwagger.tsx
@@ -1,6 +1,6 @@
 import NModel from "@/common/namespace/NModel";
 import NRouter from "@/../config/router/NRouter";
-import { Button, Space, Table } from "antd";
+import { Button, Menu, Space, Table, Tag } from "antd";
 import React, { useEffect, useRef } from "react";
 import { connect, ConnectRC, NMDGlobal, NMDProject } from "umi";
 import SelfStyle from "./LProjectSwagger.less";
@@ -34,7 +34,11 @@ const PProjectSwagger: ConnectRC<IPProjectSwaggerProps> = (props) => {
           <Button onClick={openEnterSwaggerModal}>录入</Button>
         </div>
         <div className={SelfStyle.contentWrap}>
-          <div className={SelfStyle.apiMenu}></div>
+          <div className={SelfStyle.apiMenu}>
+            <Menu mode="inline" theme="light">
+              {getApiMenu()}
+            </Menu>
+          </div>
           <div className={SelfStyle.apiDoc}></div>
         </div>
       </div>
@@ -49,6 +53,48 @@ const PProjectSwagger: ConnectRC<IPProjectSwaggerProps> = (props) => {
         })
       );
     }
+  }
+  function renderPathUrl(pathUrl: string) {
+    const list = pathUrl.split("/").filter(Boolean);
+    return (
+      <span>
+        {list.slice(0, list.length - 1).join("/")}/
+        <span className={SelfStyle.pathValue}>{list[list.length - 1]}</span>
+      </span>
+    );
+  }
+  function getApiMenu() {
+    return MDProject.domainSwaggerList.map((domainItem) => {
+      return (
+        <Menu.SubMenu key={domainItem.id} title={domainItem.domain}>
+          {Object.values(domainItem.data).map((groupItem) => {
+            return (
+              <Menu.SubMenu
+                key={groupItem.groupName}
+                title={groupItem.groupName}
+              >
+                {Object.values(groupItem.tags).map((tagItem, tagIndex) => {
+                  return (
+                    <Menu.SubMenu key={tagIndex} title={tagItem.tagName}>
+                      {Object.values(tagItem.paths).map(
+                        (pathItem, pathIndex) => {
+                          return (
+                            <Menu.Item key={pathIndex}>
+                              <Tag color="#87d068">{pathItem.method}</Tag>
+                              {renderPathUrl(pathItem.pathUrl)}
+                            </Menu.Item>
+                          );
+                        }
+                      )}
+                    </Menu.SubMenu>
+                  );
+                })}
+              </Menu.SubMenu>
+            );
+          })}
+        </Menu.SubMenu>
+      );
+    });
   }
   function openEnterSwaggerModal() {
     swaggerModalRef.current.showModal();
