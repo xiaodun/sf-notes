64094d3e9f6a763f49200c23489d1b4293bdc18f
王旭 <1947147481@qq.com>
2022-04-21 16:09:33 +0800

    迭代和角色两个TabPane1位置移动

---
+++ src/pages/iterative/RoleTabPane.tsx
@@ -0,0 +1,146 @@
+import { PageFooter } from "@/common/components/page";
+import { Button, Space, Table } from "antd";
+import React, { FC, useRef } from "react";
+import { NMDIterative } from "umi";
+import NIterative from "./NIterative";
+import SIterative from "./SIterative";
+import UCopy from "@/common/utils/UCopy";
+import EditAccountModal, {
+  IEditAccountModal,
+} from "./components/EditAccountModal";
+import AddPersonModal, { IAddPersonModal } from "./components/AddPersonModal";
+import RoleAccontInfoModal, {
+  IRoleAccontInfoModal,
+} from "./components/RoleAccontInfoModal";
+import AddEnvModal, { IAddEnvModal } from "./components/AddEnvModal";
+import AddSystemModal, { IAddSystemModal } from "./components/AddSystemModal";
+import AddRoleModal, { IAddRoleModal } from "./components/AddRoleModal";
+import ShowSystemInfoModalModal, {
+  ISystemInfoModal,
+} from "./components/SystemInfoModal";
+
+export interface IRoleTabpaneProps {
+  MDIterative: NMDIterative.IState;
+}
+const RoleTabpane: FC<IRoleTabpaneProps> = (props) => {
+  const { MDIterative } = props;
+  const addEnvModalRef = useRef<IAddEnvModal>();
+
+  const editAccountModalRef = useRef<IEditAccountModal>();
+  const addPersonModalRef = useRef<IAddPersonModal>();
+  const roleAccontInfoModalRef = useRef<IRoleAccontInfoModal>();
+  const addRoleModalRef = useRef<IAddRoleModal>();
+  const addSystemModalRef = useRef<IAddSystemModal>();
+  const showSystemInfoModalModalRef = useRef<ISystemInfoModal>();
+
+  return (
+    <div>
+      <ShowSystemInfoModalModal
+        MDIterative={MDIterative}
+        ref={showSystemInfoModalModalRef}
+      ></ShowSystemInfoModalModal>
+
+      <AddEnvModal ref={addEnvModalRef}></AddEnvModal>
+
+      <AddSystemModal ref={addSystemModalRef}></AddSystemModal>
+
+      <AddRoleModal ref={addRoleModalRef}></AddRoleModal>
+
+      <EditAccountModal
+        MDIterative={MDIterative}
+        ref={editAccountModalRef}
+        onOk={SIterative.getPersonList}
+      ></EditAccountModal>
+
+      <AddPersonModal
+        MDIterative={MDIterative}
+        ref={addPersonModalRef}
+        onOk={SIterative.getPersonList}
+      ></AddPersonModal>
+
+      <RoleAccontInfoModal
+        MDIterative={MDIterative}
+        ref={roleAccontInfoModalRef}
+      ></RoleAccontInfoModal>
+
+      <div style={{ marginTop: "20px", marginBottom: "35px" }}>
+        <Table
+          rowKey="id"
+          columns={[
+            {
+              title: "姓名",
+              key: "name",
+              dataIndex: "name",
+              render: renderNameColumn,
+            },
+            {
+              title: "角色",
+              key: "roleName",
+              dataIndex: "roleName",
+            },
+            {
+              title: "操作",
+              key: "_option",
+              render: renderOptionColumn,
+            },
+          ]}
+          dataSource={MDIterative.personList}
+          pagination={false}
+        ></Table>
+      </div>
+      <PageFooter>
+        <Button onClick={() => onShowAddEnvModal()}>添加环境</Button>
+        <Button onClick={() => onShowAddRoleModal()}>添加角色</Button>
+        <Button onClick={() => onShowAddSystemModal()}>添加系统</Button>
+        <Button onClick={() => onShowAddPersonModal()}>添加人员</Button>
+        <Button onClick={() => onShowShowSystemInfoModalModal()}>
+          系统信息
+        </Button>
+      </PageFooter>
+    </div>
+  );
+  function onShowAddSystemModal() {
+    addSystemModalRef.current.showModal();
+  }
+  function onShowShowSystemInfoModalModal() {
+    showSystemInfoModalModalRef.current.showModal();
+  }
+  function onShowAddRoleModal() {
+    addRoleModalRef.current.showModal();
+  }
+
+  function onShowAddEnvModal() {
+    addEnvModalRef.current.showModal();
+  }
+  function onShowAddPersonModal() {
+    addPersonModalRef.current.showModal();
+  }
+
+  function onShowRoleAccontInfoModal(roles: NIterative.IPerson) {
+    roleAccontInfoModalRef.current.showModal(roles);
+  }
+
+  function renderNameColumn(name: string) {
+    return <div onClick={() => UCopy.copyStr(name)}>{name}</div>;
+  }
+  function renderOptionColumn(roles: NIterative.IPerson) {
+    return (
+      <Space>
+        {roles.accountList.length > 0 && (
+          <Button type="link" onClick={() => onShowRoleAccontInfoModal(roles)}>
+            账号信息
+          </Button>
+        )}
+
+        <Button type="link" onClick={() => onShowEditAccountModal(roles)}>
+          添加账号
+        </Button>
+      </Space>
+    );
+  }
+
+  function onShowEditAccountModal(roles: NIterative.IPerson) {
+    editAccountModalRef.current.showModal(roles);
+  }
+};
+export default RoleTabpane;
