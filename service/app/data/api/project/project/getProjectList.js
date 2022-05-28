f7be9302c4e6524a289aaf03dfdfc0f697308c5c
Xiao dun <1947147481@qq.com>
2022-05-28 12:08:07 +0800

    去掉自动添加逻辑

--- service/app/data/api/project/project/getProjectList.js
+++ service/app/data/api/project/project/getProjectList.js
@@ -1,13 +1,6 @@
 (function () {
   return function (argData, argParams, external) {
-    const sfNote = argData.projectList.find((item) => item.name === "sf-notes");
-    if (!sfNote.rootPath) {
-      let pos = __dirname.indexOf("sf-notes") + "sf-notes".length;
-      sfNote.rootPath = __dirname.substring(0, pos).replace(/\\/g, "//");
-    }
     return {
-      isWrite: true,
-      data: argData,
       response: {
         code: 200,
         data: {
