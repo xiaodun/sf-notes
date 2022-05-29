d454a4edd38e732e3221d00f762f5fddff2e5749
Xiao dun <1947147481@qq.com>
2022-05-29 10:56:58 +0800

    完善自动添加sf-notes项目路径地址

--- service/app/data/api/project/project/getProjectList.js
+++ service/app/data/api/project/project/getProjectList.js
@@ -1,6 +1,13 @@
 (function () {
   return function (argData, argParams, external) {
+    const sfNote = argData.projectList.find((item) => item.name === "sf-notes");
+    if (sfNote && !sfNote.rootPath) {
+      let pos = __dirname.indexOf("sf-notes") + "sf-notes".length;
+      sfNote.rootPath = __dirname.substring(0, pos).replace(/\\/g, "//");
+    }
     return {
+      isWrite: true,
+      data: argData,
       response: {
         code: 200,
         data: {
