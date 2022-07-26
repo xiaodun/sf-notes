247575a692cfb9d96e0926f35fd6413e6ed4b8e8
王旭 <1947147481@qq.com>
2022-07-26 18:03:01 +0800

    取消生成mock写死的数据

--- service/app/data/api/project/project/getProjectSendPath.js
+++ service/app/data/api/project/project/getProjectSendPath.js
@@ -3,14 +3,10 @@
     let result = {};
     const sfMockProject = argData.projectList.find((item) => item.isSfMock);
     if (sfMockProject) {
-      if (["consultation-room-pc"].includes(argParams.projectName)) {
-        result.list = argParams.list.map(
-          (url) =>
-            `${sfMockProject.sfMock.programUrl}/${argParams.projectName}${url}`
-        );
-      } else {
-        result.errorMsg = "没有配置该项目";
-      }
+      result.list = argParams.list.map(
+        (url) =>
+          `${sfMockProject.sfMock.programUrl}/${argParams.projectName}${url}`
+      );
     } else {
       result.errorMsg = "请在项目列表中添加sf-mock";
     }
