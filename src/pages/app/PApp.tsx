2b68cb85d4475af6e43c56db70dd512f8c2e8fff
王旭 <1947147481@qq.com>
2022-08-12 15:54:06 +0800

    采用a标签进行跳转，方便控制打开行为

--- src/pages/app/PApp.tsx
+++ src/pages/app/PApp.tsx
@@ -1,25 +1,24 @@
-import { Card, Input } from 'antd';
-import React from 'react';
-import { ConnectRC } from 'umi';
-import SelfStyle from './LApp.less';
-import NApp from './NApp';
+import { Card } from "antd";
+import React from "react";
+import { ConnectRC, Link } from "umi";
+import SelfStyle from "./LApp.less";
+import NApp from "./NApp";
 export interface IPAppProps {}
-const PApp: ConnectRC<IPAppProps> = (props) => {
+const PApp: ConnectRC<IPAppProps> = () => {
   return (
     <div className={SelfStyle.main}>
       <Card>
         {NApp.list.map((item) => (
           <Card.Grid className={SelfStyle.gird} key={item.path}>
-            <div className={SelfStyle.app} onClick={() => onApp(item.path)}>
-              <h2>{item.name}</h2>
+            <div className={SelfStyle.app}>
+              <Link to={item.path} target="_blank">
+                <h2>{item.name}</h2>
+              </Link>
             </div>
           </Card.Grid>
         ))}
       </Card>
     </div>
   );
-  function onApp(path: string) {
-    props.history.push(path);
-  }
 };
 export default PApp;
