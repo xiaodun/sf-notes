bf637ed791450cca59c42191e226864a249f6a8f
Xiao dun <1947147481@qq.com>
2023-09-17 12:50:23 +0800

    优化交互，优先提示没有添加sf-mock

--- service/app/data/api/project/project/getProjectSendPath.js
+++ service/app/data/api/project/project/getProjectSendPath.js
@@ -1,22 +1,21 @@
 (function () {
   return function (argData, argParams, external) {
-    let result = {};
+    let data = {};
     const sfMockProject = argData.projectList.find((item) => item.isSfMock);
     if (sfMockProject) {
-      result.list = argParams.list.map(
+      data.list = argParams.list.map(
         (url) =>
           `${sfMockProject.sfMock.programUrl}/${argParams.projectName}${url}`
       );
+      data.success = true;
     } else {
-      result.errorMsg = "请在项目列表中添加sf-mock";
+      data.success = false;
+      data.message = "请在项目列表中添加sf-mock";
     }
     return {
       response: {
         code: 200,
-        data: {
-          success: true,
-          data: result,
-        },
+        data,
       },
     };
   };
