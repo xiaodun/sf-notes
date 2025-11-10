c200ba7b0f95365d826fa61c294773d106bb9f3b
小遁哥 <1947147481@qq.com>
2025-11-10 11:34:20 +0800

    完善错误提示

--- service/app/data/api/football/football/lifeCycle.js
+++ service/app/data/api/football/football/lifeCycle.js
@@ -31,7 +31,7 @@
                 return total;
               }, {});
             } catch (error) {
-              console
+              console.log('getGameResultList error', error);
             }
 
 
